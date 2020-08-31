b8e184c33d26910fbfd395efee26081888c1039a
dwa <nomail@example.com>
2020-08-31 19:23:04 +0300

    msg

--- __init__.py
+++ __init__.py
@@ -1,13 +1,26 @@
-from mycroft import MycroftSkill, intent_file_handler
+
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
 
 
 class Vapm(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('vapm.intent')
-    def handle_vapm(self, message):
-        self.speak_dialog('vapm')
+    @intent_handler('install.intent')
+    def handle_install(self, message):
+        package = message.data.get('package')
+        pass
+
+    @intent_handler('remove.intent')
+    def handle_remove(self, message):
+        pass
+
+    @intent_handler('search.intent')
+    def handle_remove(self, message):
+        pass
+
+    def converse(self, utterances, lang):
+        pass
 
 
 def create_skill():
