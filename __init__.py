ee86ee50f5f9774b6ec3b8f5752c12a5a0aa3d77
dwa <nomail@example.com>
2020-09-01 17:01:20 +0300

    msg

--- __init__.py
+++ __init__.py
@@ -1,25 +1,30 @@
 
 from mycroft import MycroftSkill, intent_handler
-from adapt.intent import IntentBuilder
-from mycroft.skills.context import add_context, remove_context
-from actions import search
+#from adapt.intent import IntentBuilder
+#from mycroft.skills.context import removes_context, adds_context
+#from .actions import search
+
 
 class Vapm(MycroftSkill):
+
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_handler(IntentBuilder('Search').require('search').require('package'))
+    #@adds_context('SearchResultsContext')
+    #@intent_handler(IntentBuilder('Search').require('search'))
     def handle_search(self, message):
+        print ('Ayyyyy!!!')
         package = message.data.get('package')
         results = search(package)
-        add_context('SearchResultsContext')
         self.set_context('package_name', package)
         self.set_context('result', result)
-        self.speak('Got {} results, {}, do you want to filter results?',
+        self.speak('I am not ready yet, but hey, I can do this: {}'.format(package))
+        '''self.speak('Got {} results, {}, do you want to filter results?'.format(
                 results.get_number_of_results(),
-                results.is_there_full_match() ? 'Including a full match' : 'Without a full match', expect_response=True)
+                results.is_there_full_match() ? 'Including a full match' : 'Without a full match'), expect_response=True)'''
 
-    @intent_handler(IntentBuilder('FilteringSearch').require('SearchResultsContext'.require('filter')))
+    '''
+    @intent_handler(IntentBuilder('FilteringSearch').require('SearchResultsContext').require('filter'))
     def handle_filter(self, message):
         pass
 
@@ -27,7 +32,7 @@ class Vapm(MycroftSkill):
     def handle_read(self, message):
         number = message.data.get('number')
         results = message.data.get('results')
-        number = number is None ? result.get_number_of_results() : int(number)
+        #number = number is None ? result.get_number_of_results() : int(number)
         for i in range(len(number)):
             self.speak('{}', results[i].name)
 
@@ -38,6 +43,7 @@ class Vapm(MycroftSkill):
     @intent_handler(IntentBuilder('Remove').require('remove'))
     def handle_remove(self, message):
         pass
+    '''
 
 
 def create_skill():
