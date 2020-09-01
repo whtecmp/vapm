d0d7838209950f6ee774103d990062e03557ad2d
dwa <nomail@example.com>
2020-09-01 23:03:38 +0300

    msg

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from adapt.intent import IntentBuilder
 from mycroft.skills.context import removes_context, adds_context
 from .actions import search
 
+
 def is_there_full_match(boolean):
     if boolean:
         return 'including a full match'
@@ -23,22 +24,47 @@ class Vapm(MycroftSkill):
         results = search(package_name)
         self.set_context('package_name', package_name)
         self.latest_results = results
-        self.speak('Got {} results, {}, do you want to filter results?'.format(
+        self.speak('Got {} results, {}'.format(
                 results.get_number_of_results(),
                 is_there_full_match(results.is_there_full_match())), expect_response=True)
 
     
-    @intent_handler(IntentBuilder('FilteringSearch').require('SearchResultsContext').require('filter'))
+    @intent_handler(IntentBuilder('FilteringSearch').require('SearchResultsContext').require('filter').require('filter_type').require('filter_param'))
     def handle_filter(self, message):
-        pass
+        filter_param = message.data.get('filter_param')
+        filter_type = message.data.get('filter_type')
+        packages_names = self.latest_results.get_packages_names()
+        begin = lambda name, param: name.startswith(param)
+        end = lambda name, param: name.endswith(param)
+        contain = lambda name, param: param in name
+        filters = {
+                    'begin': begin,
+                    'begins': begin,
+                    'end': end,
+                    'ends': end,
+                    'contain': contain,
+                    'contains': contain,
+                }
+        filter = lambda name: filters[filter_type](name, filter_param)
+        new_packages_names = []
+        for package_name in packages_names:
+            if filter(package_name):
+                new_packages_names.append(package_name)
+        self.latest_results.set_packages_names(new_packages_names)
+        results = self.latest_results
+        self.speak('Got {} results'.format(
+                results.get_number_of_results()), expect_response=True)
 
-    @intent_handler(IntentBuilder('ReadResults').require('SearchResultsContext').require('read').require('result').optionally('number'))
+    @intent_handler(IntentBuilder('ReadResults').require('SearchResultsContext').require('read').require('results').optionally('number'))
     def handle_read(self, message):
+        results = self.latest_results
         number = message.data.get('number')
-        results = message.data.get('results')
-        #number = number is None ? result.get_number_of_results() : int(number)
-        for i in range(len(number)):
-            self.speak('{}', results[i].name)
+        if number is None:
+            number = results.get_number_of_results()
+        else:
+            number = int(number)
+        for i in range(number):
+            self.speak('{}'.format(results.get_packages_names()[i]))
 
     @intent_handler(IntentBuilder('Install').require('install'))
     def handle_install(self, message):
--- actions.py
+++ actions.py
@@ -1,7 +1,13 @@
 
+def __set(result, packages_names):
+    result.packages_names = packages_names
+
 def search(package_name):
     result = lambda : None
-    result.get_number_of_results = lambda : 40
-    result.is_there_full_match = lambda : True
+    result.get_number_of_results = lambda : len(result.packages_names)
+    result.is_there_full_match = lambda : package_name in result.packages_names
+    result.packages_names = ['python', 'python3.6', 'libpython', 'libpython-dev', 'no-quick-yes']
+    result.get_packages_names = lambda : result.packages_names
+    result.set_packages_names = lambda packages_names: __set(result, packages_names)
     return result
 
